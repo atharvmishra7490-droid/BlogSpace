1ffeb2027215f8fb84eb429a0fd055b937506564
Atharv Mishra <atharvmishra7490@gmail.com>
2026-08-21 19:11:42 +0530

    final worldwide = delete + permanent

--- src/main/java/com/BlogSpace/BlogController.java
+++ src/main/java/com/BlogSpace/BlogController.java
@@ -1,6 +1,7 @@
 package com.BlogSpace;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -20,5 +21,16 @@ public class BlogController {
     @PostMapping
     public Blog create(@RequestBody Blog blog) {
         return repo.save(blog);
+    }
+
+    // NEW: Delete function
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBlog(@PathVariable Long id) {
+        if (repo.existsById(id)) {
+            repo.deleteById(id);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
