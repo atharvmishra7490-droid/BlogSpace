c0edeb1aa1e828625fe4eabfaa0feadfbcd3ca38
Atharv Mishra <atharvmishra7490@gmail.com>
2026-08-24 14:58:37 +0530

    Fixed Whitelabel error - added correct PageController mapping

--- src/main/java/com/BlogSpace/PageController.java
+++ src/main/java/com/BlogSpace/PageController.java
@@ -1,22 +1,33 @@
-package com.BlogSpace;
+package com.blogspace.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
+
     @GetMapping("/")
-    public String home() { return "home"; }
+    public String home(){
+        return "home";
+    }
     
     @GetMapping("/create")
-    public String create() { return "create"; }
-
+    public String create(){
+        return "create";
+    }
+    
     @GetMapping("/explore")
-    public String explore() { return "explore"; }
+    public String explore(){
+        return "explore";
+    }
     
     @GetMapping("/dashboard")
-    public String dashboard() { return "dashboard"; }
+    public String dashboard(){
+        return "dashboard";
+    }
     
     @GetMapping("/profile")
-    public String profile() { return "profile"; }
+    public String profile(){
+        return "profile";
+    }
 }
